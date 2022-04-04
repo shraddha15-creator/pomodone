2587be9f35bd555b44428b0d24cfb0fc695485ec
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-04-04 17:54:44 +0530

    homepage, navbar, style  added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import HeroImg from "../../assets/hero-img.png";
+
+import "./home.css";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<>
+			<div className="homepage-container">
+				<span className="user-greeting">Hi, Shraddha</span>
+
+				<img src={HeroImg} alt="hero-image" className="hero-img" />
+				<Link to="/tasks">
+					<button className="btn-add-task">
+						Add Task <i class="fa fa-plus-circle"></i>
+					</button>
+				</Link>
+			</div>
+		</>
+	);
 };
 
 export default Home;

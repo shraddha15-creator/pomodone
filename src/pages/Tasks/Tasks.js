025c602fe01aa1be400fe23427982dbcc3cefd39
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-04-05 11:43:53 +0530

    task page & add todo styling added

--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -1,7 +1,37 @@
 import React from "react";
+import "./task.css";
+import tasksDB from "../../tasksDB";
 
 const Tasks = () => {
-	return <div>Tasks</div>;
+	return (
+		<>
+			<div className="tasks-container">
+				<div className="title-and-btn">
+					<h2>To-Do List</h2>
+					<button className="button">
+						<i class="fa fa-plus-circle"></i>
+					</button>
+				</div>
+
+				{tasksDB &&
+					tasksDB.map(({ id, title }) => {
+						return (
+							<div className="todo-items" key={id}>
+								<h4>{title}</h4>
+								<div className="edit-delete-buttons">
+									<button className="button btn-edit-delete">
+										<i className="fas fa-edit"></i>
+									</button>
+									<button className="button btn-edit-delete">
+										<i className="fas fa-trash-alt"></i>
+									</button>
+								</div>
+							</div>
+						);
+					})}
+			</div>
+		</>
+	);
 };
 
 export default Tasks;

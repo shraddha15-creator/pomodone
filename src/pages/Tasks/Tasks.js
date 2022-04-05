cf4091bd77916e487f239403929449c4228d0be7
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-04-05 12:49:56 +0530

    feat add todo to the list implemented

--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./task.css";
 import tasksDB from "../../tasksDB";
 
 const Tasks = () => {
+	const [todos, setTodos] = useState("");
+	const [todoList, setTodoList] = useState([]);
+	const [isCompleted, setIsCompleted] = useState(true);
+
+	const changeHandler = (e) => setTodos(e.target.value);
+
+	const inputHandler = (e) => {
+		e.preventDefault();
+		setTodoList([
+			...todoList,
+			{ id: uuidv4(), title: todos, isCompleted: false },
+		]);
+		setTodos("");
+	};
+
 	return (
 		<>
 			<div className="tasks-container">
 				<div className="title-and-btn">
 					<h2>My To-Do</h2>
-					<div className="input-and-btn">
-						<input
-							type="text"
-							className="add-todo-input"
-							placeholder="Add todo"
-						/>
-						<button className="button">
-							<i class="fa fa-plus-circle"></i>
-						</button>
-					</div>
+					<form onSubmit={inputHandler}>
+						<div className="input-and-btn">
+							<input
+								type="text"
+								className="add-todo-input"
+								placeholder="Add todo"
+								value={todos}
+								required
+								onChange={changeHandler}
+							/>
+							<button className="button">
+								<i class="fa fa-plus-circle"></i>
+							</button>
+						</div>
+					</form>
 				</div>
 
 				{tasksDB &&
-					tasksDB.map(({ id, title }) => {
+					todoList.map(({ id, title }) => {
+						console.log(todoList);
 						return (
 							<div className="todo-items" key={id}>
 								<div>{title}</div>
